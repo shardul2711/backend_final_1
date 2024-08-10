6d7c551d1c17f17b41428dc0fab0f008bed6a8af
Shardul <sharduldhanokar@gmail.com>
2024-08-10 23:16:02 +0530

    my 5th commit

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const path = require("path");
 const ownersRouter = require("./routes/ownersRouter");
 const productsRouter = require("./routes/productsRouter");
 const usersRouter = require("./routes/usersRouter");
-const index = require("./routes/index");
+const indexRouter = require("./routes/index");
+
+require("dotenv").config();
 
 const db = require("./config/mongoose-connection")
 
@@ -18,7 +20,7 @@ app.set("view engine" , "ejs");
 app.use("/owners" , ownersRouter);
 app.use("/products" , productsRouter);
 app.use("/users" , usersRouter);
-app.use("/" , index);
+app.use("/" , indexRouter);
 
 
 
